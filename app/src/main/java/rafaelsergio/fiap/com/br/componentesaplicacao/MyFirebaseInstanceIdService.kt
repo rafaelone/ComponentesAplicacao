eb7c25bef23c9773c0e307b940e940e35d8e8e54
rafael <logonrmlocal@fiap.com.br>
2018-10-09 22:36:06 -0300

    add firebase e push notification

---
+++ app/src/main/java/rafaelsergio/fiap/com/br/componentesaplicacao/MyFirebaseInstanceIdService.kt
@@ -0,0 +1,14 @@
+package rafaelsergio.fiap.com.br.componentesaplicacao
+
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+class MyFirebaseInstanceIdService: FirebaseInstanceIdService(){
+    override fun onTokenRefresh() {
+        super.onTokenRefresh()
+
+        val token = FirebaseInstanceId.getInstance().token
+        Log.i("TOKEN", token)
+    }
+}
---
+++ app/src/main/java/rafaelsergio/fiap/com/br/componentesaplicacao/MyFirebaseMessagingService.kt
@@ -0,0 +1,14 @@
+package rafaelsergio.fiap.com.br.componentesaplicacao
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService(){
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+        Log.d("NOTIFICACAO", "Message Notification Bod: "+ p0?.notification?.body)
+        Log.d("NOTIFICACAO", "Message Notification Bod: "+ p0?.notification?.title)
+
+    }
+}
