0a909e5ff912ab0b77bbbd40322ff51077929915
rafael <logonrmlocal@fiap.com.br>
2018-10-09 22:55:00 -0300

    fazendo aparecer notification

--- app/src/main/java/rafaelsergio/fiap/com/br/componentesaplicacao/MyFirebaseMessagingService.kt
+++ app/src/main/java/rafaelsergio/fiap/com/br/componentesaplicacao/MyFirebaseMessagingService.kt
@@ -9,6 +9,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService(){
         super.onMessageReceived(p0)
         Log.d("NOTIFICACAO", "Message Notification Bod: "+ p0?.notification?.body)
         Log.d("NOTIFICACAO", "Message Notification Bod: "+ p0?.notification?.title)
-
+        NotificationUtils.showNotification(this, 21321, "PUSH",
+                "PUSH",  p0?.notification?.title!!,  p0?.notification?.body!! )
     }
 }
